2bef8e8611fa11609c5da4cf6ed2179a44eb1e9d
MatheusPires12 <matheusinhopires2010@hotmail.com>
2024-08-04 15:35:48 -0300

    criando a tela

--- Jogo.py
+++ Jogo.py
@@ -1,5 +1,30 @@
 import pygame
 from pygame.locals import *
+from sys import exit
 
-pygame.init()
+class Jogo:
+    def __init__(self):
+        pygame.init()
+        self.largura = 600
+        self.altura = 400
+        self.tela = pygame.display.set_mode((self.largura, self.altura))
+        pygame.display.set_caption("Jogo da Cobrinha")
+        self.relogio = pygame.time.Clock()
+        self.fonte = pygame.font.SysFont('arial', 40, True, True)
+
+    def executar(self):
+        while True:
+            self.relogio.tick(10)
+            self.tela.fill((255, 255, 255))
+            pygame.display.update()
+            self.lidar_com_eventos()
+    def lidar_com_eventos(self):
+        for evento in pygame.event.get():
+            if evento.type == QUIT:
+                pygame.quit()
+                exit()
+jogo = Jogo()
+jogo.executar()
+
+        
 
